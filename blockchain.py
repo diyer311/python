d47e3a5dc219da36724733dbeb044031b24e4375
fissionboy <52163012@qq.com>
2019-12-20 09:58:01 +0800

    Update blockchain.py
    update some stuff

--- blockchain.py
+++ blockchain.py
@@ -163,8 +163,8 @@ blockchain = Blockchain()
 
 @app.route('/mine', methods=['GET'])
 def mine_block():
-    last_block = blockchain.last_block
-    last_proof = last_block['proof']
+    latest_block = blockchain.last_block
+    last_proof = latest_block['proof']
     proof = blockchain.proof_of_work(last_proof)
 
     blockchain.new_transaction(
